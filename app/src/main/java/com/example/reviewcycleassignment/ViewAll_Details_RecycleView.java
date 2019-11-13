e83f6bd580bd8f70416242f8a8962711c75483d3
Sudin101 <sudin.sudin@outlook.com>
2019-11-13 17:41:15 +0545

    Initial commit

---
+++ app/src/main/java/com/example/reviewcycleassignment/ViewAll_Details_RecycleView.java
@@ -0,0 +1,41 @@
+package com.example.reviewcycleassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.reviewcycleassignment.Model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAll_Details_RecycleView extends AppCompatActivity {
+    RecyclerView recyclerView;
+//    List<User> users = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all__details__recycle_view);
+
+
+       recyclerView=findViewById(R.id.rvusers);
+
+//       users.add(new User("momo","male","19-8-2016","nepal","5656","sd@g.c","yjy"));
+
+        Intent intent = getIntent();
+
+        final List<User> users = (List<User>)intent.getSerializableExtra("userlist");
+
+
+        MyRvAdapter adapter = new MyRvAdapter(users,this);
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(this);
+
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
